d78c105e889cbbb024b993d39039e6a6acb84163
mateusdev3400 <mateusdev3400@gmail.com>
2023-12-18 10:15:07 -0300

    feat: variables and list of products in the array

---
+++ js/mock/listProductsStock.js
@@ -0,0 +1,6 @@
+// Mocked list of products in stock 
+export const listProductsStock = [
+    { id: 1, title: "Fone de ouvido", price: 1000, amount: 0, }, 
+    { id: 2, title: "Celular", price: 1400, amount: 0, }, 
+    { id: 3, title: "Óculos", price: 5000, amount: 0, }
+];
---
+++ js/variables/variables.js
@@ -0,0 +1,10 @@
+export let product = document.querySelector("select");
+export let amount = document.querySelector("input");
+
+export let elementShowItens = document.querySelector("#list-added-products");
+export let elementShowItensTotal = document.querySelector("#list-total-values");
+
+export let buttonAddItem = document.querySelector("#button-add");
+export let buttonRemoveItem = document.querySelector("#button-remove");
+
+export let listProductsAdded = [];
